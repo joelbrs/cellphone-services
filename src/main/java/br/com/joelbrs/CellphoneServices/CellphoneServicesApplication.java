edd326f8f020eca7ea47c6d443df1144c2977338
Joel Filho <joelfilho02812@gmail.com>
2023-10-20 12:09:14 -0300

    project created

---
+++ src/main/java/br/com/joelbrs/CellphoneServices/CellphoneServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelbrs.CellphoneServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellphoneServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellphoneServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joelbrs/CellphoneServices/CellphoneServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelbrs.CellphoneServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellphoneServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
